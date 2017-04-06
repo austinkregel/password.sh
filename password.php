47f920ea7f1c61a1cb27445384861bf7d8ff5de8
Austin Kregel <austin@kbcomp.co>
2017-04-06 14:03:43 -0400

    Moved scripts to their own folder

--- password.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo substr(str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz1234567890~!@#$%^&*()_+-='), 0, !empty($argv[1]) ? $argv[1] : 32) ."\n";
--- password.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -z $1 ]; then 
-    LENGTH_OF_PASSWORD=32
-else
-    LENGTH_OF_PASSWORD=$1
-fi
-
-< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-$LENGTH_OF_PASSWORD};echo;
