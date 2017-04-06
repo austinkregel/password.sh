3afea788ea0a548e184e55b5e10cee3a4065c19d
Austin Kregel <austin@kbcomp.co>
2017-04-06 13:44:23 -0400

    Removed puncutation flag from python script

--- password.py
+++ password.py
@@ -6,13 +6,5 @@ import sys
 totalLength = int(sys.argv[1]) if len(sys.argv) > 1 else 32
 
 
-if len(sys.argv) > 2:
-     usePunctuation = sys.argv[2]
-else:
-     usePunctuation = ''
-
-if usePunctuation == 'true' or usePunctuation == '1':
-     print ''.join(random.SystemRandom().choice(string.hexdigits + "~!@#$%^&*()_+-=") for _ in range(totalLength))
-else:
-     print ''.join(random.SystemRandom().choice(string.hexdigits) for _ in range(totalLength))
+print ''.join(random.SystemRandom().choice(string.hexdigits) for _ in range(totalLength))
 
