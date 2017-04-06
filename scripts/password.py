f50329422fe24b8e06b54723fa581004eed4d110
Thomas Schultz <daspecster@gmail.com>
2017-04-06 14:14:56 -0400

    Add Py3 print statement parens, and add more characters.

--- scripts/password.py
+++ scripts/password.py
@@ -6,5 +6,5 @@ import sys
 totalLength = int(sys.argv[1]) if len(sys.argv) > 1 else 32
 
 
-print ''.join(random.SystemRandom().choice(string.hexdigits) for _ in range(totalLength))
+print(''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits + string.string.punctuation) for _ in range(totalLength)))
 
